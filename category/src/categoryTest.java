bc75300e48465f55d4ebad5b0b0ef51563a8a950
David Hu <david.hu.411@gmail.com>
2023-10-20 01:14:14 -0400

    added some tests

--- category/src/categoryTest.java
+++ category/src/categoryTest.java
@@ -1,27 +1,15 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpRequest.BodyPublisher;
-import java.net.http.HttpResponse.BodyHandlers;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import org.json.simple.*;
-
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
-
-public class tests {
-
+public class categoryTest {
     @Test
     public void testing_post() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
@@ -33,7 +21,6 @@ public class tests {
         HttpResponse<String> response_str = client.send(request, HttpResponse.BodyHandlers.ofString());
         assertEquals(201, response_str.statusCode());
         System.out.println(response_str.body());
-
     }
     @Test
     public void get_categories() throws IOException, InterruptedException {
@@ -63,5 +50,5 @@ public class tests {
         assertEquals(201, response_str.statusCode());
         System.out.println(response_str.body());
     }
-    
 }
+
